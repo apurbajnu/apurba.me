7b31b34d329c92f123926133e3d41094fae74c77
Apurba Podder <apurbapodder@Apurbas-Mac-mini.local>
2026-03-31 01:27:58 +0600

    feat: migrate to AstroWind template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,87 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import icon from 'astro-icon';
+import compress from 'astro-compress';
+import node from '@astrojs/node';
+
+import astrowind from './vendor/integration';
+import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
-  integrations: [mdx()],
-  // ADD THIS SECTION:
+
   server: {
     host: '0.0.0.0',
-    port: 4000
-  }
+    port: 4500
+  },
+
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    mdx(),
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': [
+          'template',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+        ],
+      },
+    }),
+    partytown({
+      config: { forward: ['dataLayer.push'] },
+    }),
+    compress({
+      CSS: true,
+      HTML: {
+        'html-minifier-terser': {
+          removeAttributeQuotes: false,
+        },
+      },
+      Image: false,
+      JavaScript: true,
+      SVG: false,
+      Logger: 1,
+    }),
+    astrowind({
+      config: './src/config.yaml',
+    }),
+  ],
+
+  image: {
+    domains: ['cdn.pixabay.com'],
+  },
+
+  markdown: {
+    remarkPlugins: [readingTimeRemarkPlugin],
+    rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin],
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
