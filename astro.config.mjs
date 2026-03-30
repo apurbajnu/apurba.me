5f03ce9f4391baec07eabeffeb04ef990c1b5f6a
Apurba Podder <apurbapodder@Apurbas-Mac-mini.local>
2026-03-30 17:11:38 +0600

    Initial Astro Setup

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import node from '@astrojs/node';
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [mdx()]
+});
